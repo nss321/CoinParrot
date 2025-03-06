53d3bdb56a148b4f5ce04b05621ea4f6fe8d8f4b
BAE <lexon1209@gmail.com>
2025-03-06 23:34:38 +0900

    chore: folder structure

--- CoinParrot/Source/Present/ViewController.swift
+++ CoinParrot/Source/Present/ViewController.swift
@@ -7,11 +7,15 @@
 
 import UIKit
 
+import RealmSwift
+import RxRelay
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
 
 
