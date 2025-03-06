9dd0b4cb746d5b61de3201862b801962b4b217ff
BAE <lexon1209@gmail.com>
2025-03-06 23:54:28 +0900

    design: 폰트 및 컬러 에셋 추가

---
+++ CoinParrot/Source/Extension/UIFont+.swift
@@ -0,0 +1,20 @@
+//
+//  UIFont+.swift
+//  CoinParrot
+//
+//  Created by BAE on 3/6/25.
+//
+
+import UIKit
+
+extension UIFont {
+    /// bold, 12
+    static func boldPrimary() -> UIFont { return UIFont.boldSystemFont(ofSize: 12) }
+    /// bold, 9
+    static func boldSecondary() -> UIFont { return UIFont.boldSystemFont(ofSize: 9) }
+    /// regular, 12
+    static func regularPrimary() -> UIFont { return UIFont.systemFont(ofSize: 12) }
+    /// regular, 9
+    static func regularSecondary() -> UIFont { return UIFont.systemFont(ofSize: 9) }
+}
+
