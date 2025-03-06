e02cd7e586c48f1a809b5259553f5aa9d85751b3
BAE <lexon1209@gmail.com>
2025-03-06 23:57:53 +0900

    chore: add common margin

---
+++ CoinParrot/Source/Extension/UIResponder+.swift
@@ -0,0 +1,21 @@
+//
+//  UIResponder+.swift
+//  CoinParrot
+//
+//  Created by BAE on 3/6/25.
+//
+
+import UIKit
+
+extension UIResponder {
+    
+    /// 12
+    var smallMargin: Int { 12 }
+    
+    /// 16
+    var mediumMargin: Int { 16 }
+    
+    /// 20
+    var largeMargin: Int { 20 }
+    
+}
