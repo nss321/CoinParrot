8ec8a4e83fbb4c27b528fff0e647eb76ef9eafb3
BAE <lexon1209@gmail.com>
2025-03-08 19:40:10 +0900

    feat: adding specified edge's border method implement

---
+++ CoinParrot/Source/Extension/CALayer+.swift
@@ -0,0 +1,34 @@
+//
+//  CALayer+.swift
+//  CoinParrot
+//
+//  Created by BAE on 3/8/25.
+//
+
+import UIKit
+
+extension CALayer {
+    func addEdgeBorder(_ edges: [UIRectEdge], color: UIColor, width: CGFloat) {
+        for edge in edges {
+            let border = CALayer()
+            switch edge {
+            case UIRectEdge.top:
+                border.frame = CGRect.init(x: 0, y: 0, width: frame.width, height: width)
+                break
+            case UIRectEdge.bottom:
+                border.frame = CGRect.init(x: 0, y: frame.height - width, width: frame.width, height: width)
+                break
+            case UIRectEdge.left:
+                border.frame = CGRect.init(x: 0, y: 0, width: width, height: frame.height)
+                break
+            case UIRectEdge.right:
+                border.frame = CGRect.init(x: frame.width - width, y: 0, width: width, height: frame.height)
+                break
+            default:
+                break
+            }
+            border.backgroundColor = color.cgColor;
+            self.addSublayer(border)
+        }
+    }
+}
