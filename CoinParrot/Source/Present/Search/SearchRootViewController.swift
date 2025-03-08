ea0c5cf21437e5001bf31f65b7e28535b53afa70
BAE <lexon1209@gmail.com>
2025-03-08 19:38:45 +0900

    chore: 파일 추가
    - CALayer Extension, SearachTab VCs

---
+++ CoinParrot/Source/Present/Search/SearchRootViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SearchViewController.swift
+//  CoinParrot
+//
+//  Created by BAE on 3/8/25.
+//
+
+import UIKit
+
+final class SearchRootViewController: BaseViewController {
+    
+    
+    
+    override func configLayout() {
+        
+    }
+    
+    override func configView() {
+//        navigationItem.backBarButtonItem?.image = UIImage(systemName: "arrow.left")
+//        navigationItem.backBarButtonItem?.image
+        print(#function)
+    }
+}
